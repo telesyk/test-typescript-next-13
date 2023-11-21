c33b61839413f95b70ad05ce09aee8434b77bf1d
telesyk <telesyk89@gmail.com>
2023-11-21 17:44:38 +0200

    Update loding UI

--- app/loading.tsx
+++ app/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0">
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 animate-pulse">
-        <div className="w-[200vw] h-[200vw] bg-gradient-to-t from-lime-500 via-indigo-500 to-pink-600 animate-spin"></div>
+        <div className="w-[45vw] h-[45vw] rounded-full animate-spin shadow-inner shadow-lime-500 bg-transparent"></div>
       </div>
     </div>
   )
